8e7ead4bf16d01043a41eb36ff97096f20b36baa
kshyamki <kshyamki@DIN80000684.corp.capgemini.com>
2018-08-06 11:16:26 +0530

    fdsv

---
+++ Z/src/main/java/project/Z/App.java
@@ -0,0 +1,14 @@
+package project.Z;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("kishan bow bow!! ");
+    }
+}
